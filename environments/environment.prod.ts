05bde17df5b8ed00dfd848bc0e8d6654bfd3c41e
Araccelli Zevallos <araccelli.zevallos@gmail.com>
2022-08-04 19:23:17 -0500

    build(frontend): cambia URL de backend pruebas en el entorno

--- environments/environment.prod.ts
+++ environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: true,
-    apiURL: 'https://backend-gestion-configuracion.herokuapp.com/api/v1/'
+    apiURL: 'https://backend-gestion-configuracion.herokuapp.com/api/v1/',
+    apiBase: 'https://backend-gestion-configuracion.herokuapp.com'
   };
   
